fa962f5a0a0b36635cf646b4e2ec5c84c2c3ca21
Priyansh Sharma <piyuindia4@gmail.com>
2022-03-15 21:32:03 +0530

    Version1.0

---
+++ app.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const md5 = require('md5');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost:27017/peopleDB', {
+    useNewUrlParser: true
+});
+
+const peopleSchama = new mongoose.Schema({
+    fname: String,
+    lname: String,
+    email: String,
+    subject: String,
+    message: String
+});
+
+const People = mongoose.model('People', peopleSchama);
+
+const subscriberSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const Subscriber = new mongoose.model("Subscriber", subscriberSchema);
+
+
+
+
+
+app.get('/', function (req, res) {
+    res.render("home");
+})
+
+app.get('/work', function (req, res) {
+    res.render("work");
+})
+
+
+app.get('/about', function (req, res) {
+    res.render("about");
+})
+
+app.get('/contact', function (req, res) {
+    res.render("contact");
+})
+
+app.get('/play', function (req, res) {
+    res.render("play")
+})
+
+app.get('/login', function (req, res) {
+    res.render("login");
+});
+
+app.get('/register', function (req, res) {
+    res.render("register");
+});
+
+app.post('/contact', function (req, res) {
+    const people = new People({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        subject: req.body.subject,
+        message: req.body.message
+    })
+
+
+    people.save(function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+
+            res.redirect("/contact");
+        }
+    })
+})
+
+
+app.post('/register',function(req,res){
+    const subscriber = new Subscriber({
+        username:req.body.username,
+        password: md5(req.body.password)
+    })
+
+    subscriber.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/");
+        }
+    })
+})
+
+app.post(function(req,res){
+    
+    const username = req.body.username;
+    const password = md5(req.body.password);
+
+    Subscriber.findOne({username:username},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                if(foundUser.password===password){
+                    res.redirect("/")
+                }
+            }
+        }
+    })
+})
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server started at port 3000");
+})
